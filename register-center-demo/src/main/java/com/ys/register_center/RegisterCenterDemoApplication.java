5115e94f59e0e7aa81cbf82fcabe2af095545300
yusheng <yushengruohui@gmail.com>
2020-05-04 23:54:26 +0800

    创建注册中心模块

---
+++ register-center-demo/src/main/java/com/ys/register_center/RegisterCenterDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ys.register_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterCenterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterCenterDemoApplication.class, args);
+	}
+
+}
---
+++ register-center-demo/src/test/java/com/ys/register_center/RegisterCenterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.register_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterCenterDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
